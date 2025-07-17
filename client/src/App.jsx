e19540a1303a655d05d4e12a7c322166564e7c5c
kshitiz <kshitizmainaly3334@gmail.com>
2025-07-17 22:06:45 +0545

    create frontend backend folder structure

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <>
+<Navbar/>
+
+<h1>hello kshitiz</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+   
+      <ul className="flex gap-2.5 text-2xl text-amber-600 justify-between bg-black">
+       <li>kshitiz</li>
+       <li>kishor</li>
+       <li>mausam</li> 
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
